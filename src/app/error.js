cf44352b7d7d838a56d2807471c9cfaa244370ec
abhimanyusingh88 <singabhimanyu9794@gmail.com>
2026-01-29 12:38:12 +0530

    resolved some production bugs

--- src/app/error.js
+++ src/app/error.js
@@ -16,7 +16,7 @@ export default function Error({ error, reset }) {
       />
 
       <h1 className="text-3xl font-semibold text-white">Something went wrong!</h1>
-      <p className="text-lg text-gray-300">{error.message}</p>
+      {/* <p className="text-lg text-gray-300">{error.message}</p> */}
 
       <button
         className="inline-block bg-accent-500 text-primary-800 px-6 py-3 text-lg"
